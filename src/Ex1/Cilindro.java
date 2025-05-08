31edd75785f3645733104fec291b9aefd3ad2c0d
Nicolas Padovam <nicolaspadovam@gmail.com>
2025-05-08 09:49:24 -0300

    indo pro intervalo

--- src/Ex1/Cilindro.java
+++ src/Ex1/Cilindro.java
@@ -1,5 +1,13 @@
 package Ex1;
 
-public class Cilindro {
+public class Cilindro extends FormaGeometrica {
+
+    private double altura;
+
+    public Cilindro(int coordenadaX, int coordenadaY, double raio, double altura) {
+        super(coordenadaX, coordenadaY, raio);
+        this.altura = altura;
+    }
+
 
 }
